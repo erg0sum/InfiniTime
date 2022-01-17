04aa9102234ff64c05847c382c8f0e49d0cc7514
erg0sum <adam.hotz@gmail.com>
2022-01-17 00:45:38 -0500

    Update WatchFaceDigital.cpp

--- src/displayapp/screens/WatchFaceDigital.cpp
+++ src/displayapp/screens/WatchFaceDigital.cpp
@@ -253,7 +253,7 @@ void WatchFaceDigital::Refresh() {
         }
       }
 
-      lv_label_set_text_fmt(label_time, "%s:%s:%s", hoursChar, minutesChar, secondsChar);
+      lv_label_set_text_fmt(label_time, "%s:%s", hoursChar, minutesChar);
 
       if (settingsController.GetClockType() == Controllers::Settings::ClockType::H12) {
         lv_obj_align(label_time, lv_scr_act(), LV_ALIGN_IN_LEFT_MID, 0, 0);
